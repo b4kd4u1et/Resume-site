be6559659c38f6d553a99b876890451e0dcb63a2
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-11-16 21:27:37 +0600

    u

--- content/scripts/achivementsScript.js
+++ content/scripts/achivementsScript.js
@@ -1,5 +1,14 @@
 let data = JSON.parse(achivements);
-function input_creater() {
+
+for (var i = 0; i < data.length; i++) {
+  let selector = document.getElementById('selector');
+  let div = document.createElement('div');
+  div.id = 'div' + '_' + i;
+  let vendor_label = document.createElement('label');
+  vendor_label.id = 'label' + '_' + i;
+  vendor_label.innerHTML = data[i].vendor;
+  div.insertAdjacentElement('afterend', vendor_label);
+  for (var j = 0; j < data[i].badges.length; j++) {
     let input = document.createElement('input');
     input.id = 'input' + '_' + i + '_' + j;
     input.type = 'radio';
@@ -11,17 +20,5 @@ function input_creater() {
     label.innerHTML = data[i].badges[j].badge_name;
     input.insertAdjacentElement('afterend', label);
     div.insertAdjacentElement('afterend', input);
-}
-
-for (var i = 0; i < data.length; i++) {
-  let selector = document.getElementById('selector');
-  let div = document.createElement('div');
-  div.id = 'div' + '_' + i;
-  let vendor_label = document.createElement('label');
-  vendor_label.id = 'label' + '_' + i;
-  vendor_label.innerHTML = data[i].vendor;
-  div.insertAdjacentElement('afterend', vendor_label);
-  for (var j = 0; j < data[i].badges.length; j++) {
-    input_creater();
   }
 }
