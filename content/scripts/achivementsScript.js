7b5fc668be683c82afbb4b9fa886b60a63a20b82
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-11-16 12:40:02 +0600

    u

--- content/scripts/achivementsScript.js
+++ content/scripts/achivementsScript.js
@@ -1,9 +1,17 @@
 let data = JSON.parse(achivements);
 function input_creater() {
     let selector = document.getElementById('selector');
-    let input = document.createElement('input').id = i;
+    let input = document.createElement('input');
+    input.id = 'input'+'_'+i+'_'+j;
+    input.name = 'href';
+    input.value = data[i].badges[j].link;
+    let label = document.createElement('label');
+    label.id = 'label'+'_'+i+'_'+j;
+    label.for = input.getAttribute('id');
+    label.in
+    input.insertAdjacentElement('afterend', label);
     selector.insertAdjacentElement('afterend', input);
-    
+
 }
 for (var i = 0; i < data.length; i++) {
 
