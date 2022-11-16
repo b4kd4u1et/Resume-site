7957f0d4bbd14aaec61a9d7583addbfb6aa5b944
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-11-16 12:56:22 +0600

    u

--- content/scripts/achivementsScript.js
+++ content/scripts/achivementsScript.js
@@ -1,18 +1,26 @@
 let data = JSON.parse(achivements);
 function input_creater() {
     let input = document.createElement('input');
-    input.id = 'input'+'_'+i+'_'+j;
+    input.id = 'input' + '_' + i + '_' + j;
     input.name = 'href';
     input.value = data[i].badges[j].link;
     let label = document.createElement('label');
-    label.id = 'label'+'_'+i+'_'+j;
+    label.id = 'label' + '_' + i + '_' + j;
     label.for = input.getAttribute('id');
     label.innerHTML = data[i].badges[j].badge_name;
     input.insertAdjacentElement('afterend', label);
-    selector.insertAdjacentElement('afterend', input);
+    div.insertAdjacentElement('afterend', input);
 }
 
 for (var i = 0; i < data.length; i++) {
   let selector = document.getElementById('selector');
   let div = document.createElement('div');
+  div.id = 'div' + '_' + i;
+  let vendor_label = document.createElement('label');
+  vendor_label.id = 'label' + '_' + i;
+  vendor_label.innerHTML = data[i].vendor;
+  div.insertAdjacentElement('afterend', vendor_label);
+  for (var j = 0; j < data[i].badges.length; j++) {
+    input_creater();
+  }
 }
