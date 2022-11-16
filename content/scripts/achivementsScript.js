50b1211d8bdd3862588c0fb586dc6c1b3fef1726
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-11-16 12:19:57 +0600

    u

--- content/scripts/achivementsScript.js
+++ content/scripts/achivementsScript.js
@@ -1,1 +1,10 @@
-let data = JSON.parse(achivements)
+let data = JSON.parse(achivements);
+function input_creater() {
+    let selector = document.getElementById('selector');
+    let input = document.createElement('input').id = i;
+    selector.insertAdjacentElement('afterend', input);
+    
+}
+for (var i = 0; i < data.length; i++) {
+
+}
