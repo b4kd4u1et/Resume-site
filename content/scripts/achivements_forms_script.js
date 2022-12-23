69a803a4a2f44aabf25a45827daa6df0c41a293d
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-12-23 21:21:30 +0600

    clearing

--- content/scripts/achivements_forms_script.js
+++
@@ -1,4 +0,0 @@
-function selector_func(data){
-  let frame = document.getElementById("achivement_frame");
-  frame.src = data;
-};
--- content/scripts/me.html.script.js
+++
@@ -1,2 +0,0 @@
-let biography = JSON.parse(data);
-document.getElementById('bio').innerHTML = biography.bio;
