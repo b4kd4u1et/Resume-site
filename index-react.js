bdc0af183463b9984f20c3cac783aba4a3face2d
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2023-02-08 21:28:35 +0600

    u

--- index-react.js
+++
@@ -1,7 +0,0 @@
-let rootNode = document.getElementById("app");
-
-let root = ReactDOM.createRoot(rootNode);
-
-root.render(
-  <h1>Hello World!</h1>,
-);
---
+++ index-react.jsx
@@ -0,0 +1,7 @@
+let rootNode = document.getElementById("app");
+
+let root = ReactDOM.createRoot(rootNode);
+
+root.render(
+  <h1>Hello World!</h1>,
+);
